3aeffee74c145a23fd4a3c81396e07c73a27df66
Joachim Jablon <joachim.jablon@people-doc.com>
2018-04-20 13:53:41 +0200

    Reworked the lib for basic usage

--- vault_cli/vault_python_api.py
+++ vault_cli/vault_python_api.py
@@ -1,73 +1,100 @@
 #! /usr/bin/env python
 
 import requests
-import json
 
 from urllib.parse import urljoin
-from functools import reduce
-
-s = requests.Session()
-# s.verify = False
-
-headers = {'X-Vault-Token': ''}
-url = 'http://127.0.0.1:8200/v1/'
-path = 'secret/'
 
 
-def userpass_authentication(username, password):
-    d = {"password": password}
-    r = s.post(url + 'auth/userpass/login/' + username, json=d, headers={})
-    json_response = r.json()
+class VaultSession(object):
+    def __init__(self, url, verify, base_path,
+                 certificate=None, token=None, username=None, password_file=None):
 
-    if r.status_code == requests.codes.ok:
-        s.headers.update({'X-Vault-Token': json_response.get('auth').get('client_token')})
+        self.session = requests.Session()
+        self.session.verify = verify
+        self.url = urljoin(url, "v1/")
+        self.base_path = base_path
+
+        if token:
+            self.session.headers.update({'X-Vault-Token': token})
+        elif certificate:
+            certificate_authentication(self.session, certificate.read())
+        elif username:
+            if not password_file:
+                raise ValueError('Cannot use username without password file')
+            password = password_file.read().decode("utf-8").strip()
+            userpass_authentication(self.session, self.url, username, password)
+        else:
+            raise ValueError("No authentication method supplied")
+
+        if 'X-Vault-Token' not in self.session.headers:
+            raise ValueError("Failed authentication")
+
+    def full_url(self, path=None):
+        url = urljoin(self.url, self.base_path)
+        if path:
+            return urljoin(url, path)
+        return url
+
+
+def create_session(verify):
+    return
+
+
+def userpass_authentication(session, url, username, password):
+    data = {"password": password}
+    response = session.post(url + 'auth/userpass/login/' + username,
+                            json=data, headers={})
+    json_response = response.json()
+
+    if response.status_code == requests.codes.ok:
+        session.headers.update(
+            {'X-Vault-Token': json_response.get('auth').get('client_token')})
     else:
-        print('Wrong username or password')
+        raise ValueError('Wrong username or password')
 
 
 # TODO
-def certificate_authenticate(cert):
+def certificate_authentication(session, cert):
     pass
 
 
-def get_secret(key):
-    r = s.get(reduce(urljoin, (url, path, key)))
-    json_response = r.json()
+def get_secret(session, url):
+    response = session.get(url)
+    json_response = response.json()
 
-    if r.status_code == requests.codes.ok:
+    if response.status_code == requests.codes.ok:
         return json_response.get('data').get('value')
 
 
-def get_secrets(key):
-    r = s.get(reduce(urljoin, (url, path, key)))
-    json_response = r.json()
+def get_secrets(session, url):
+    response = session.get(url)
+    json_response = response.json()
 
-    if r.status_code == requests.codes.ok:
+    if response.status_code == requests.codes.ok:
         return json_response.get('data')
 
 
-def list_secrets():
-    r = s.get(reduce(urljoin, (url, path.rstrip('/'), '?list=true')))
-    json_response = r.json()
+def list_secrets(session, url):
+    response = session.get(url.rstrip('/') + '?list=true')
+    json_response = response.json()
 
-    if r.status_code == requests.codes.ok:
+    if response.status_code == requests.codes.ok:
         return json_response.get('data').get('keys')
 
 
-def put_secret(key, data):
-    headers["Content-Type"] = "application/json"
-    r = s.put(reduce(urljoin, (url, path, key)), json=data)
+def put_secret(session, url, data):
+    response = session.put(url, json=data)
 
-    if r.status_code == 204:
+    if response.status_code == 204:
         return 'ok'
     else:
-        return ','.join(r.json().get('errors')), r.status_code
+        return ','.join(response.json()['errors']), response.status_code
 
 
-def delete_secret(key):
-    r = s.delete(reduce(urljoin, (url, path, key)))
+def delete_secret(session, url):
+    response = session.delete(url)
 
-    if r.status_code == 204:
+    if response.status_code == 204:
         return 'ok'
     else:
-        return ','.join(r.json().get('errors')), r.status_code
+        return ','.join(response.json()['errors']), response.status_code
